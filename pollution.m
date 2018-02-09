9c89d761816d755859408d24f05da12fb6826233
pisihara <summerresearch14@gmail.com>
2018-02-09 16:26:11 -0600

    Add files via upload

---
+++ pollution.m
@@ -0,0 +1,16 @@
+M = xlsread('minilab.xlsx'); %import data
+y = M(:,1); %SO2 level
+x = M(:,2); %temperature
+
+%MATLAB method for linear regression
+X = [ones(length(x),1),x]; %add ones to prepare for linear regression
+coeff = X\y; %Calculate coefficients for model: m = coeff(2), b = coeff(1)
+
+xplot = 45:80; %range of x values from data
+yplot = coeff(2) * xplot + coeff(1); %create y values from found coefficients
+
+%plot model data over real data
+scatter(x,y)
+hold on %for multiple plots in one graph
+plot(xplot,yplot,'--');
+hold off

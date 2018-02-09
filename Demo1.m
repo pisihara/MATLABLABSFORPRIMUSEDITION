15bfed309f8caa4ed27ab129d54c48cd69c9f2fb
pisihara <summerresearch14@gmail.com>
2018-02-09 16:03:01 -0600

    Add files via upload

---
+++ Demo1.m
@@ -0,0 +1,13 @@
+%% ARITHMETIC OPERATIONS
+clear all; close all; 
+a=(5+3)- (2+1)  % addition and subtraction
+b=(5*2) + (8/4)  % multiplication
+c=2^(-1) % exponentiation
+d= a+b/c ;  %% Order of operations (),^,*/,+- semi-colon suppresses output
+
+
+
+
+
+
+
